02ff953f8cc9a9ff329ec31ddc9e30b5b6522f3f
ganireddy651 <gajarla.ganesh1995@gmail.com>
2023-06-18 04:44:52 +0000

    gghhgdwed

---
+++ src/components/CountriesList/index.js
@@ -0,0 +1,40 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class CountriesList extends Component {
+  state = {isVisit: false}
+
+  onClickVisit = () => {
+    this.setState({isVisit: true})
+  }
+
+  render() {
+    const {eachCountry} = this.props
+    const {name, imageUrl, isVisited} = eachCountry
+    return (
+      <>
+        <div className="list-container">
+          <li className="list">
+            <p className="country">{name}</p>
+            {isVisited ? (
+              <p className="visited-btn" type="button">
+                Visited
+              </p>
+            ) : (
+              <button
+                onClick={this.onClickVisit}
+                className="button"
+                type="button"
+              >
+                Visit
+              </button>
+            )}
+          </li>
+        </div>
+      </>
+    )
+  }
+}
+
+export default CountriesList
---
+++ src/components/VisitedCountries/index.js
@@ -0,0 +1,5 @@
+import './index.css'
+
+const VisitedCountries = () => <h1>Ganesh</h1>
+
+export default VisitedCountries
